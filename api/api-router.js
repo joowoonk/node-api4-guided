797e9506bbcfbb709c01729c7b333c59c9d8088a
Golden Eagle <joowoon.kang@biola.edu>
2020-05-07 10:48:47 -0700

    added restricted on get

--- api/api-router.js
+++ api/api-router.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
 
-router.get("/shouts", (req, res, next) => {
+router.get("/shouts", restricted, (req, res, next) => {
   Shouts.find()
     .then((shouts) => {
       res.status(200).json(shouts);
